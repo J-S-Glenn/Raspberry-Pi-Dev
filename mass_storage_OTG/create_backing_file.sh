5ac5f53cab51463041e961e2640b382a07998c18
John S Glenn Jr <152113379+jsglenn-flc@users.noreply.github.com>
2023-11-29 00:00:49 -0800

    Test flow

--- mass_storage_OTG/create_backing_file.sh
+++ mass_storage_OTG/create_backing_file.sh
@@ -1,7 +1,20 @@
-IMG_SIZE_GB="50GiB"
+IMG_SIZE="50GiB"
 IMG_PATH="/piusb.bin"
+VOL_LABEL="PiUSB"
+GMO_PARAM="stall=0 ro=1"
 
-sudo fallocate -l $IMG_SIZE_GB  $IMG_PATH
+# Remove the file in case it already exists
+rm $IMG_PATH
 
-sudo mkfs -t ext4 $IMG_PATH
+# Create new file that is IMG_SIZE large
+sudo fallocate -l $IMG_SIZE  $IMG_PATH
+
+# Create Partition
+echo "start=1, type=7" | sfdisk $IMG_PATH
+
+# Create Filesystem
+sudo mkfs.ntfs -L $VOL_LABEL -F $IMG_PATH --fast
+
+# Expose USB device
+sudo modprobe g_mass_storage file=$IMG_PATH
 
